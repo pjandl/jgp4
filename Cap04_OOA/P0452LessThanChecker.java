b9a364b30229314144c0bdbba9d7c20e29484cd8
PJandl <pjandl@gmail.com>
2020-10-01 14:18:58 -0300

    Delete P0452LessThanChecker.java
    Numeração incorreta do exemplo.

--- Cap04_OOA/P0452LessThanChecker.java
+++
@@ -1,7 +0,0 @@
-public class P0452LessThanChecker implements P0451Checker<Boolean,Double> {
-	@Override
-	public Boolean check(Double left, Double right) {
-		// comparação dos argumentos
-		return left < right;
-	}
-}
